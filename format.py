3fd4e122010c24512f25f5905b868418f8f0dfe6
patrick-naylor <patrickknaylor@gmail.com>
2022-11-12 17:05:05 -0800

    Updated format script

--- format.py
+++ format.py
@@ -30,6 +30,24 @@ def main():
             train[f'last{bound}'] = train.apply(partial(similar, c='protein_sequence', w=start_wild, b=bound, side='end'), axis=1)
 
         return train
+	col = wild_type
+	length = len(col)
+	tenths = [int((length/10)*(x+1)) for x in range(10)]
+	wild_list = []
+	for idx, ten in enumerate(tenths):
+	    col_tenth = col[idx:ten]
+	    col_dict = {}
+	    for letter in col_tenth:
+	        if letter in col_dict:
+	            col_dict[letter] += 1
+	        else:
+	            col_dict[letter] = 1
+	    for letter in letters:
+	        if letter not in col_dict:
+	            col_dict[letter] = 0
+	    for key, value in col_dict.items():
+	        col_dict[key] = value/(length/10)
+	    wild_list.append(col_dict)
 
     def tenths(train, letters):
         col = train['protein_sequence']
@@ -51,11 +69,11 @@ def main():
                 col_dict[key] = value/(length/10)
             tenth_list.append(col_dict)
         labels = np.arange(0, 110, 10)
-        for idx, t in enumerate(tenth_list):
+        for idx, (t, w) in enumerate(zip(tenth_list, wild_list)):
             for l in letters:
-                train[f'{labels[idx]}-{labels[idx+1]}{l}_count'] = t[l]
+                train[f'{labels[idx]}-{labels[idx+1]}{l}_count'] = t[l] - w[l]
         return train
-        
+
     df = df.apply(partial(tenths, letters=letters), axis=1)
     df = match_columns(wild_type, df)
 
